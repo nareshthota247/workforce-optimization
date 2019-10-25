c0b8a75898d1798ed2a2cc3c6e03e296a9721038
nareshthota247 <49022372+nareshthota247@users.noreply.github.com>
2019-10-25 19:38:01 +0530

    more test cases

--- src/test/java/com/servicepartner/one/workforce/optimization/WorkforceOptimizationApplicationTests.java
+++ src/test/java/com/servicepartner/one/workforce/optimization/WorkforceOptimizationApplicationTests.java
@@ -1,6 +1,9 @@
 package com.servicepartner.one.workforce.optimization;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import java.math.BigDecimal;
 
@@ -9,10 +12,12 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.servicepartner.one.workforce.optimization.model.CleaningTasks;
 import com.servicepartner.one.workforce.optimization.model.StaffingLevel;
 import com.servicepartner.one.workforce.optimization.service.OptimizerService;
 
@@ -45,4 +50,110 @@ class WorkforceOptimizationApplicationTests {
 		StaffingLevel staffingLevel = optimizerService.optimiseForBuilding(21, 10, 6);
 		assertThat(staffingLevel).isEqualTo(new StaffingLevel(1, 2));
 	}
+	
+	@Test
+	public void when_NegitiveSeniorthenConstrainViolation() throws Exception {
+		Integer[] rooms=new Integer[]{35, 21, 17, 28};
+		
+		CleaningTasks ct=new CleaningTasks();
+		ct.setRooms(rooms);
+		ct.setSenior(-8);
+		ct.setJunior(6);
+		 
+		mockMvc.perform(post("/api/optimize").contentType(MediaType.APPLICATION_JSON)
+				.content(mapper.writeValueAsBytes(ct)))
+				.andExpect(status().isBadRequest())
+				.andExpect(jsonPath("$.senior").value("Senior must be a positive number")).andReturn();
+	}
+	
+	@Test
+	public void when_ZeroSeniorthenConstrainViolation() throws Exception {
+		Integer[] rooms=new Integer[]{35, 21, 17, 28};
+		
+		CleaningTasks ct=new CleaningTasks();
+		ct.setRooms(rooms);
+		ct.setSenior(0);
+		ct.setJunior(6);
+		 
+		mockMvc.perform(post("/api/optimize").contentType(MediaType.APPLICATION_JSON)
+				.content(mapper.writeValueAsBytes(ct)))
+				.andExpect(status().isBadRequest())
+				.andExpect(jsonPath("$.senior").value("Senior must be a positive number")).andReturn();
+	}
+	
+	@Test
+	public void when_NegitiveJuniorthenConstrainViolation() throws Exception {
+		Integer[] rooms=new Integer[]{35, 21, 17, 28};
+		
+		CleaningTasks ct=new CleaningTasks();
+		ct.setRooms(rooms);
+		ct.setSenior(8);
+		ct.setJunior(-6);
+		  
+		mockMvc.perform(post("/api/optimize").contentType(MediaType.APPLICATION_JSON)
+				.content(mapper.writeValueAsBytes(ct)))
+				.andExpect(status().isBadRequest())
+				.andExpect(jsonPath("$.junior").value("Junior must be a positive number")).andReturn();
+	}
+	
+	@Test
+	public void when_ZeroJuniorthenConstrainViolation() throws Exception {
+		Integer[] rooms=new Integer[]{35, 21, 17, 28};
+		
+		CleaningTasks ct=new CleaningTasks();
+		ct.setRooms(rooms);
+		ct.setSenior(8);
+		ct.setJunior(0);
+		  
+		mockMvc.perform(post("/api/optimize").contentType(MediaType.APPLICATION_JSON)
+				.content(mapper.writeValueAsBytes(ct)))
+				.andExpect(status().isBadRequest())
+				.andExpect(jsonPath("$.junior").value("Junior must be a positive number")).andReturn();
+	}
+	
+	@Test
+	public void when_ZeroRoomSizethenConstrainViolation() throws Exception {
+		Integer[] rooms=new Integer[]{0, 21, 17, 28};
+		
+		CleaningTasks ct=new CleaningTasks();
+		ct.setRooms(rooms);
+		ct.setSenior(8);
+		ct.setJunior(6);
+		  
+		mockMvc.perform(post("/api/optimize").contentType(MediaType.APPLICATION_JSON)
+				.content(mapper.writeValueAsBytes(ct)))
+				.andExpect(status().isBadRequest())
+				.andExpect(jsonPath("$.errorDesc").value("Room size cannot be Zero or Negitive")).andReturn();
+	}
+	
+	@Test
+	public void when_NegitiveRoomSizethenConstrainViolation() throws Exception {
+		Integer[] rooms=new Integer[]{-35, 21, 17, 28};
+		
+		CleaningTasks ct=new CleaningTasks();
+		ct.setRooms(rooms);
+		ct.setSenior(8);
+		ct.setJunior(6);
+		  
+		mockMvc.perform(post("/api/optimize").contentType(MediaType.APPLICATION_JSON)
+				.content(mapper.writeValueAsBytes(ct)))
+				.andExpect(status().isBadRequest())
+				.andExpect(jsonPath("$.errorDesc").value("Room size cannot be Zero or Negitive")).andReturn();
+	}
+	
+	@Test
+	public void when_ZeroRoomCountthenConstrainViolation() throws Exception {
+		Integer[] rooms=new Integer[]{};
+		
+		CleaningTasks ct=new CleaningTasks();
+		ct.setRooms(rooms);
+		ct.setSenior(8);
+		ct.setJunior(6);
+		  
+		mockMvc.perform(post("/api/optimize").contentType(MediaType.APPLICATION_JSON)
+				.content(mapper.writeValueAsBytes(ct)))
+				.andExpect(status().isBadRequest())
+				.andExpect(jsonPath("$.rooms").value("size must be between 1 and 100")).andReturn();
+	}
+	
 }
